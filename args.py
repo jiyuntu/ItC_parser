b7ce0f177e20744ab14318ccad471e7fa72562a2
jiyuntu <wendy3925@gmail.com>
2020-02-25 22:58:13 +0800

    2020-02-25 debug

--- args.py
+++ args.py
@@ -3,13 +3,13 @@ from datetime import datetime
 
 
 def get_args():
-    # TODO: Add --start-date, --end-date and --output arguments
-    #       Convert the two dates to datetime objects
+	# TODO: Add --start-date, --end-date and --output arguments
+	#       Convert the two dates to datetime objects
 	parser = argparse.ArgumentParser()
 	parser.add_argument("--start-date")
 	parser.add_argument("--end-date")
 	parser.add_argument("--output")
-    args = parser.parse_args()
-	args.start_date = datetime.striptime(args.start_date, '%Y-%m-%d')
-	args.end_date = datetime.striptime(args.end_date, '%Y-%m-%d')
+	args = parser.parse_args()
+	args.start_date = datetime.strptime(args.start_date, '%Y-%m-%d')
+	args.end_date = datetime.strptime(args.end_date, '%Y-%m-%d')
 	return args
--- main.py
+++ main.py
@@ -7,7 +7,7 @@ if __name__ == '__main__':
 	crawler = Crawler()
 	contents = crawler.crawl(args.start_date, args.end_date)
     # TODO: write content to file according to spec
-	with open(output,'w') as csv_out:
+	with open(args.output,'w') as csv_out:
 		for obj in contents:
 			outstr = f'{str(obj[0])},"{obj[1]}","{obj[2]}"\n'
-			f.write(outstr)
+			csv_out.write(outstr)
